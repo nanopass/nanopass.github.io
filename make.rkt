3e8512f7d55f60786d69d2578bb9e9f1bd1bf37e
Leif Andersen <leif@leifandersen.net>
2016-01-06 00:49:09 -0500

    Fixed a few bugs in make.rkt

--- make.rkt
+++ make.rkt
@@ -8,13 +8,7 @@
 (define-runtime-path git (let ([f (find-executable-path "git")])
                            (or f (error "git could not be found"))))
 
-;; Generate html files
-(for ([f (in-list files)])
-  (with-output-to-file (path-replace-suffix f ".html")
-    #:exists 'replace
-    (lambda () (system* (find-exe) f))))
-
-;; Push html files to origin in master branch
+;; Check error conditions if deploying
 (parameterize ([current-directory project-root-dir])
   (define current-branch
     (make-parameter
@@ -24,14 +18,20 @@
 
   ;; Error if repo is not in commitable state
   (when (non-empty-string? (with-output-to-string
-                                 (lambda () (system* git "status" "--porcelain"))))
+                             (lambda () (system* git "status" "--porcelain"))))
     (error "Please commit changes before deploying"))
   (when (equal? (current-branch) "master")
     (error "Cannot deploy in master branch"))
 
-  ;; Set current branch to be master
-  (system* git "checkout" "-B" "master")
 
+  ;; Generate html files
+  (for ([f (in-list files)])
+    (with-output-to-file (path-replace-suffix f ".html")
+      #:exists 'replace
+      (lambda () (system* (find-exe) f))))
+
+  ;; Push html files to origin in master branch
+  (system* git "checkout" "-B" "master")
   (for ([f (in-list (dict-keys html-file-table))])
     (system* git "add" f))
   (system* git "commit" "-m" "\"Automatic commit\"")
