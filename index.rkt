03542c52aa13ff65759978e28cf3ba2692326db6
Leif Andersen <leif@leifandersen.net>
2016-01-08 14:30:13 -0500

    Add blurb and button.

--- index.rkt
+++ index.rkt
@@ -3,5 +3,13 @@
 @require["templates.rkt"]
 
 @page[#:title "Home"]{
-  @div[class: "container"]{@p{Hello World!}}
-}
+  @div[class: "jumbotron"]{
+    @div[class: "container"]{
+      @div[class: "splash"]{
+        @h2{Clean Compiler Creation Language}
+        @p{@b{The Nanopass Framework} is an embedded domain specific
+          for creating compilers that focuses on creating small passes
+          and many intermediate representations.
+          Nanopass reduces the boilerplate required to create
+          compilers making them easier to understand and maintain.}}
+      @center{@a[class: "btn-oval btn btn-default btn-lg" href: "http://github.com/nanopass" role: "button"]{Get Started}}}}}
