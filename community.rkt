65df829896a05efa41402f4d8a982bd46cc38b04
Leif Andersen <leif@leifandersen.net>
2016-01-12 18:08:18 -0500

    Update community page.

--- community.rkt
+++ community.rkt
@@ -3,5 +3,11 @@
 @require["templates.rkt"]
 
 @page[#:title "Community"]{
-  @div[class: "container"]{@p{Community webpage}}
-}
+  @div[class: "jumbotron"]{
+    @div[class: "container"]{@h1{Community}}}
+  @div[class: "container"]{
+    @h2{Mailing Lists}
+    @p{There is currently one Nanopass mailing list. Usage and
+      implementation questions, as well as feature requests, should be
+      directed there.}
+    @ul{@li{@a[href: "https://groups.google.com/forum/#!forum/nanopass-framework"]{Nanopass Mailing List}}}}}
